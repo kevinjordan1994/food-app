0adc3177b9107effe265be985da8da307f26d90b
FingerPrintGames <kevinjordan199449@gmail.com>
2022-08-09 07:53:09 -0400

    Add cart button animation

--- src/components/Header/CartButton.js
+++ src/components/Header/CartButton.js
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Cart from "../../store/cart";
 import styles from "../UI/Button.module.css";
 
 export default function CartButton(props) {
   const cart = useContext(Cart);
+  const [buttonStyles, changeButtonStyles] = useState(`${styles.cart_button}`);
 
   const showCart = () => {
     cart.toggleCartModal();
@@ -15,8 +16,15 @@ export default function CartButton(props) {
     })
     .reduce((a, b) => a + b, 0);
 
+  useEffect(() => {
+    changeButtonStyles(`${styles.cart_button} ${styles.bump}`);
+    setTimeout(() => {
+      changeButtonStyles(`${styles.cart_button}`);
+    }, 500);
+  }, [amountOfItemsInCart]);
+
   return (
-    <button onClick={showCart} className={styles.cart_button}>
+    <button onClick={showCart} className={buttonStyles}>
       Your Cart <span>{amountOfItemsInCart}</span>
     </button>
   );
